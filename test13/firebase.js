e06094c77cbb251decd49b072a8f36eff9de740d
chloesettle <50433519+chloesettle@users.noreply.github.com>
2019-07-01 21:31:33 -0700

    Add files via upload

---
+++ test13/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDU4_DbN3kO9-E2PE5u-jtd4fxYycNb_qs",
+    authDomain: "spacebuds-invaders.firebaseapp.com",
+    databaseURL: "https://spacebuds-invaders.firebaseio.com",
+    projectId: "spacebuds-invaders",
+    storageBucket: "spacebuds-invaders.appspot.com",
+    messagingSenderId: "299366500827",
+    appId: "1:299366500827:web:cac78c0ce7a4ee54"
+  };
+
+  firebase.initializeApp(firebaseConfig);
